f29a934e84257e9cee006fc8c72010252414b0d2
Rozen Raymond <rozen.raymond@lendi.com.au>
2019-07-13 09:51:46 +1000

    added homepage and form components

---
+++ src/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'antd/es/button';
+
+function HomePage() {
+  return (
+   <div>
+       This is the homepage
+   </div>
+  );
+}
+
+export default HomePage;
