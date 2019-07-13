47dc2e8e003002c947837d74507c356f633b3005
Maryam.Mostajir <maryam.mostajir@sbs.com.au>
2019-07-13 13:00:03 +1000

    add backup copy in case any data is missing

--- src/ConfirmationPage/index.js
+++ src/ConfirmationPage/index.js
@@ -12,7 +12,12 @@ const ConfirmationPage = (props) => {
      <Capsule src={timeCapsule} alt="time capsule" />
      <TextContainer> 
         <CardHeader>Thanks for your message!</CardHeader>
-        <Text>We’ve stored it away in a time capsule until {props.sentDate}. When the time comes, we’ll send it back to your {props.sendMethod}: {props.sendAddress}.</Text>
+        { props.sentDate && props.sendMethod && props.sendAddress &&
+            <Text>We’ve stored it away in a time capsule until {props.sentDate}. When the time comes, we’ll send it back to your {props.sendMethod}: {props.sendAddress}.</Text>
+          || 
+            <Text>We’ve stored it away in a time capsule until the time you have specified. We will send it to you when the time comes.</Text>
+  
+        }
         <Text>We hope you enjoy reading it!</Text>
      </TextContainer>
      <Button type="primary" onClick={() => {history.push('/create-letter')}}>Write another letter</Button>
