f0af8c1033214a6a89675a8fb8a7e7b8e95d2492
Maryam.Mostajir <maryam.mostajir@sbs.com.au>
2019-07-13 12:50:18 +1000

    add styling and capsule png

--- src/ConfirmationPage/index.js
+++ src/ConfirmationPage/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Button from 'antd/es/button';
+import { Container, TextContainer, Text, CardHeader, Capsule } from './index.style';
+import timeCapsule from '../Timecapsule.png'
 
 const ConfirmationPage = (props) => {
   const {
     history
   } = props;
   return (
-   <div>
-     <h1>Header</h1>
-     <p>Thanks for your letter! We’ve stored it away in a time capsule until {props.sentDate}.</p>
-     <p>When the time comes, we’ll send it back to your {props.sendMethod}: {props.sendAddress}.</p>
-     <p>We hope you enjoy reading it when the time comes!</p>
-     <Button type="primary" onClick={() => {history.push('/create-letter')}}>Create another letter!</Button>
-   </div>
+   <Container>
+     <Capsule src={timeCapsule} alt="time capsule" />
+     <TextContainer> 
+        <CardHeader>Thanks for your message!</CardHeader>
+        <Text>We’ve stored it away in a time capsule until {props.sentDate}. When the time comes, we’ll send it back to your {props.sendMethod}: {props.sendAddress}.</Text>
+        <Text>We hope you enjoy reading it!</Text>
+     </TextContainer>
+     <Button type="primary" onClick={() => {history.push('/create-letter')}}>Write another letter</Button>
+   </Container>
   );
 }
 
---
+++ src/ConfirmationPage/index.style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    border: 1px solid red;
+    text-align: center;
+    margin: 0 auto;
+`
+
+export const Capsule = styled.img`
+    max-width: 510px;
+`
+
+export const CardHeader = styled.h1`
+    font-family: "Ubuntu";
+    font-size: 32px;
+`
+
+export const TextContainer = styled.div`
+    border: 1px solid green;
+    max-width: 700px;
+    margin: 30px auto;
+`
+
+export const Text = styled.p`
+    font-family: "Ubuntu";
+    font-size: 24px;
+`
