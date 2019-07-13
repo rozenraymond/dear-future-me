728f3d295abb13a4c6668572f19a8dc8d1359414
Maryam.Mostajir <maryam.mostajir@sbs.com.au>
2019-07-13 13:38:15 +1000

    change backup message

--- src/ConfirmationPage/index.js
+++ src/ConfirmationPage/index.js
@@ -15,8 +15,7 @@ const ConfirmationPage = (props) => {
         { props.sentDate && props.sendMethod && props.sendAddress &&
             <Text>We’ve stored it away in a time capsule until {props.sentDate}. When the time comes, we’ll send it back to your {props.sendMethod}: {props.sendAddress}.</Text>
           || 
-            <Text>We’ve stored it away in a time capsule until the time you have specified. We will send it to you when the time comes.</Text>
-  
+            <Text>We've stored it away and will send it to you when the time comes.</Text>
         }
         <Text>We hope you enjoy reading it!</Text>
      </TextContainer>
